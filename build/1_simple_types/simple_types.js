b0e073fb61d2c465db7aba1fc652a11a8913c293
Donovan Said <donovan.s.said@gmail.com>
2024-05-31 14:45:25 +0100

    Initial commit. (#1)
    ## [0.1.0]
    
    ### Added
    
    * Initial release

---
+++ build/1_simple_types/simple_types.js
@@ -0,0 +1,8 @@
+"use strict";
+//Simple Types
+// Explicit Types
+let firstName = "Donovan";
+console.log(`${firstName}`);
+// Implicit Types
+let lastName = "Said";
+console.log(`${lastName}`);
---
+++ src/0_hello_world/hello_world.ts
@@ -0,0 +1,6 @@
+function sayHello(name: string) {
+    console.log(`Hello, ${name}!`);
+}
+
+const user = "Donovan";
+sayHello(user);
